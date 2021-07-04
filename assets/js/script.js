a328ea288920c1d58b331d14070665c4734ffb3b
jjcourtney <jcourtneywork@gmail.com>
2021-07-04 16:38:42 +0100

    updated createFiveDay

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,17 +12,38 @@ let kevlinToCelsius = tempKel => tempKel - 273.15; // -273.15 kelvin = 0 deg cel
 fetch(url)
 .then(reponse => reponse.json())
 .then(data => {
-    // for(let i = 0; i < data.daily.length; i++){
-    // // console.log(data.daily);
-    // // createCard(i, data.daily[i]); 
 
-
-    // console.log(data.daily[i]);
-    // }
     createFiveDay(data.daily);
     console.log(data.daily)
     
 })
+
+const intToDay = dayAsInt => {
+    let day = "Weather";
+    switch(dayAsInt){
+        case 0:
+            day = "Sunday";
+            break;
+          case 1:
+            day = "Monday";
+            break;
+          case 2:
+            day = "Tuesday";
+            break;
+          case 3:
+            day = "Wednesday";
+            break;
+          case 4:
+            day = "Thursday";
+            break;
+          case 5:
+            day = "Friday";
+            break;
+          case 6:
+            day = "Saturday";
+    }
+    return day;
+}
 
 const createFiveDay = (daysData) => {
     for (let i = 0; i < 5; i++){
@@ -35,31 +56,18 @@ const createFiveDay = (daysData) => {
         let currentHumidity = daysData[i].humidity;
         let currentIcon = daysData[i].weather[0].icon;
         let currentIconSrc = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`;
+        let date = new Date(daysData[i].dt * 1000);
+        let day = intToDay(date.getDay())
 
         $(currentDiv).children(".max")[0].textContent = `Max: ${currentMax}°C`
         $(currentDiv).children(".min")[0].textContent = `Min: ${currentMin}°C`
         $(currentDiv).children(".wind")[0].textContent = `Wind: ${currentWind}mph`
         $(currentDiv).children(".humidity")[0].textContent = `Humidity: ${currentHumidity}%`
         $(currentDiv).children(".weather-img")[0].src = currentIconSrc;
+        $(currentDiv).children(".day")[0].textContent = day;
     }
 }
  
-
-
-// let createCard = (day, daily) =>  {
-
-//     const dateObject = new Date(daily.dt * 1000)
-//     let day1 = dateObject.toLocaleString("en-US", {weekday: "long"})
-//     let tempMax = Math.round(kevlinToCelsius(daily.temp.max));
-//     let tempMin = Math.round(kevlinToCelsius(daily.temp.min));
-//     let icon = daily.weather[0].icon;
-//     let description = daily.weather[0].description;
-//     let newEl = $("<div></div>")
-//     .attr("id", `day-${day}`)
-//     .html(`<h1>${day1}</h1><img src="http://openweathermap.org/img/wn/${icon}@2x.png" title="${description}"> Max: ${tempMax} Min: ${tempMin}`);
-//     mainEl.append(newEl);
-// }
-
 /*
 weather:
 Thunderstorm
