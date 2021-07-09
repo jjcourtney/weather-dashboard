e18e25275a2834546f60b6c13c9473e7aba21ea8
jjcourtney <jcourtneywork@gmail.com>
2021-07-09 23:28:44 +0100

    refactoring variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,22 @@
 const apiKey = "31b1715eea27e8546c5192709d456eb7";
-let mainEl = $("#main")
+const mainEl = $("#main")
+
+// onload defaults
 let lat = "52.5";
 let lon = "-1.95";
-
 let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&exclude=hourly`;
 let cityName = "Birmingham";
-let kevlinToCelsius = tempKel => tempKel - 273.15; // -273.15 kelvin = 0 deg cel
+let firstLoad = true;
+
+// elements required
 const searchBtn = $("#search-btn");
 const searchDiv = $("#search");
 const selectorDiv = $("#selector");
 const prevSearchDiv = $("#previous-searches");
-let firstLoad = true;
+
+
+
+const kevlinToCelsius = tempKel => tempKel - 273.15; // -273.15 kelvin = 0 deg cel
 
 const isInList = (currentId = "") => {
     if($(`[data-city-id="${currentId}"]`).length){
